5bba64c7d4d213f4cdb97da7d06ad75d53a13d69
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-03 08:21:06 +0900

    Update anomal_ddpm_classifier_guidance.sh

--- script/anomal_ddpm_classifier_guidance.sh
+++ script/anomal_ddpm_classifier_guidance.sh
@@ -8,17 +8,14 @@ obj_name='transistor'
 trigger_word='transistor'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="from_trained_pe_local_global_from_pretrained_vae_global_matching"
+file_name="monai_trial"
 
 anomal_source_path="../../../MyData/anomal_source"
-network_weights="../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/local_all_crossattn_pe/models/epoch-000009.safetensors" \
 
 python ../anomal_ddpm_classifier_guidance.py \
  --output_dir "../../result/${bench_mark}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
  --start_epoch 0 --max_train_epochs 30 \
- --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" \
- --network_weights ${network_weights} \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" --anomal_only_on_object \
  --anomal_source_path "${anomal_source_path}" \
