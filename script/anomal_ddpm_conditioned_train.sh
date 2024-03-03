6293f07270dfe110c3921ab0639de13711458144
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-04 06:45:06 +0900

    Update anomal_ddpm_conditioned_train.sh

--- script/anomal_ddpm_conditioned_train.sh
+++ script/anomal_ddpm_conditioned_train.sh
@@ -10,7 +10,7 @@ anomal_source_path="../../../MyData/anomal_source"
 
 # having anomal image only on object
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../anomal_ddpm_conditioned_train.py \
  --output_dir "../../result/${bench_mark}/${obj_name}/${file_name}" \
  --start_epoch 0 --max_train_epochs 100 \
