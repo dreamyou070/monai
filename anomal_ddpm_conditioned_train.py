42e0754897b8e1cf65240947481885d7a5511a4d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-09 17:41:47 +0900

    Update anomal_ddpm_conditioned_train.py

--- anomal_ddpm_conditioned_train.py
+++ anomal_ddpm_conditioned_train.py
@@ -201,17 +201,18 @@ def main(args):
             # [6] epoch final
 
         accelerator.wait_for_everyone()
+        """
         #if is_main_process:
             #ckpt_name = get_epoch_ckpt_name(args, "." + args.save_model_as, epoch + 1)
             #save_model(args, ckpt_name, accelerator.unwrap_model(network), save_dtype)
-            """
+            
             if position_embedder is not None:
                 position_embedder_base_save_dir = os.path.join(args.output_dir, 'position_embedder')
                 os.makedirs(position_embedder_base_save_dir, exist_ok=True)
                 p_save_dir = os.path.join(position_embedder_base_save_dir,
                                           f'position_embedder_{epoch + 1}.safetensors')
                 pe_model_save(accelerator.unwrap_model(position_embedder), save_dtype, p_save_dir)
-            """
+        """
 
     #accelerator.end_training()
 
